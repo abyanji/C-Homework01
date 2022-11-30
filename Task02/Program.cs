54c8cd692b833eb6a096c3124b999261fdc4322b
abyanji <a.byanjil99@gmail.com>
2022-11-30 17:48:59 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите три числа");
+int[] numbers = new int[3];
+numbers[0] = int.Parse(Console.ReadLine());
+numbers[1] = int.Parse(Console.ReadLine());
+numbers[2] = int.Parse(Console.ReadLine());
+int max = numbers[0];
+int index = 0;
+
+while (index < numbers.Length)
+{
+    if (max < numbers[index])
+    {
+        max = numbers[index];
+    }
+    index++;
+    
+}
+Console.WriteLine($"Максимальное число = {max}");
