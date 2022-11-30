2f44d81ce77030375498fc87b2904230f843c6e8
abyanji <a.byanjil99@gmail.com>
2022-11-30 17:14:36 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+
+if (A > B)
+{
+    Console.WriteLine($"Max = {A}");
+}
+else
+{
+    Console.WriteLine($"Max = {B}");
+}
